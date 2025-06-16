75d6daa5caba79bc604272194c4851bd2de3a181
Siddharth Chudasama <168630164+Sid-9510@users.noreply.github.com>
2025-06-16 03:47:25 +0000

    datatypes

---
+++ Js-basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+let Name = "Siddharth";
+let age = 21;
+let isLoggedIn = true;
+let state;
+
+// number => range=2 to the pwr of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbole => unique
+
+// object
+
+console.table([typeof Name, typeof age, typeof isLoggedIn, typeof state]);
+console.table([typeof undefined, typeof null]);
+
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object

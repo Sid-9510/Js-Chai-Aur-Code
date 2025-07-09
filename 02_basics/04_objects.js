e3331e0d2480fe971359a37eb316c7f69d25baaf
Siddharth Chudasama <168630164+Sid-9510@users.noreply.github.com>
2025-07-09 14:59:48 +0000

    json talks

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -8,31 +8,31 @@ instaUser.isLoggedIn = false;
 // console.log(instaUser);
 
 const regularUser = {
-    usr1 : {
-        name : "Sid",
-        email : "sid@gmail.com",
-        city : "Ahmedabad",
-        work : {
-            des : "Software dev",
-            office : "Central mall" 
+    usr1: {
+        name: "Sid",
+        email: "sid@gmail.com",
+        city: "Ahmedabad",
+        work: {
+            des: "Software dev",
+            office: "Central mall"
         }
     },
-    usr2 : {
-        name : "Hrdx",
-        email : "hrdx@gmail.com",
-        city : "Gandhinagar",
-        work : {
-            des : "Managing drctr",
-            office : "Central mall"
+    usr2: {
+        name: "Hrdx",
+        email: "hrdx@gmail.com",
+        city: "Gandhinagar",
+        work: {
+            des: "Managing drctr",
+            office: "Central mall"
         }
     },
-    usr3 : {
-        name : "Aki",
-        email : "aki@gmail.com",
-        city : "Surat",
-        work : {
-           des : "Data Anlst",
-            office : "Central mall" 
+    usr3: {
+        name: "Aki",
+        email: "aki@gmail.com",
+        city: "Surat",
+        work: {
+            des: "Data Anlst",
+            office: "Central mall"
         }
     }
 }
@@ -40,29 +40,29 @@ const regularUser = {
 
 // console.log(regularUser.usr1.work.des);
 
-const obj1 = {1:'a', 2:'b', 3:'c'}
-const obj2 = {4:'d', 5:'e', 6:'f'}
-const obj3 = {7:'g', 8:'h', 9:'i'}
+const obj1 = { 1: 'a', 2: 'b', 3: 'c' }
+const obj2 = { 4: 'd', 5: 'e', 6: 'f' }
+const obj3 = { 7: 'g', 8: 'h', 9: 'i' }
 
 // const allObj = {obj1, obj2, obj3}; // By this way we'll get objects in object
 // const allObj = Object.assign({}, obj1, obj2, obj3); // By this way we'll get all the entities of objects in one object
 
-const allObj = {...obj1, ...obj2, ...obj3} // This Spread method is modern and easy way to combine all the objects
+const allObj = { ...obj1, ...obj2, ...obj3 } // This Spread method is modern and easy way to combine all the objects
 
 // console.log(allObj);
 
 const usr = [
     {
-        id : 1001,
-        email : "abc@gmail.com"
+        id: 1001,
+        email: "abc@gmail.com"
     },
     {
-        id : 1002,
-        email : "abd@gmail.com"
+        id: 1002,
+        email: "abd@gmail.com"
     },
     {
-        id : 1003,
-        email : "abe@gmail.com"
+        id: 1003,
+        email: "abe@gmail.com"
     },
 ];
 
@@ -75,3 +75,35 @@ const usr = [
 
 // console.log(instaUser.hasOwnProperty("isLoggedIn"));
 // console.log(instaUser.hasOwnProperty("isLogged"));
+
+/*** Destructuring of an Object ***/
+
+const course = {
+    courseName: "Js in Hindi",
+    coursePrice: 999,
+    courseInstructure: "Hitesh"
+}
+
+// console.log(course.courseInstructure);
+
+const { courseInstructure: instructure } = course; // Here at right we take a name of object we want property from, and at left in curly brackets we take a property name and also give another name to that properety to make it easy to use
+
+console.log(instructure);
+
+/*** Json Format for API (Both keys and values are in string form) ***/
+
+// {
+//     "name" : "Hitesh",
+//     "coursename" : "Js in Hindi",
+//     "Price" : "Free"
+// }
+
+/*** Sometimes it comes in the form of arrays with objects inside ***/
+
+// [
+//     {},
+//     {},
+//     {},
+//     {},
+//     {},
+// ]

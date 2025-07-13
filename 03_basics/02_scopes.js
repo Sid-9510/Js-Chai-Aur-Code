800c8652dcec22e77c8b01c4580f300730ff420b
Siddharth Chudasama <168630164+Sid-9510@users.noreply.github.com>
2025-07-13 07:04:51 +0000

    Scope level and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -12,3 +12,41 @@ if (true) {
 // console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const userName = "Sid";
+    function two(){
+        const website = "Youtube";
+        console.log(userName);
+        
+    }
+    // console.log(website);
+    two();
+
+};
+// one();
+
+if (true) {
+    const username = "Siddharth";
+    if (username === "Siddharth") {
+        const website = "Youtube";
+        // console.log(username + website);
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+// console.log(addone(5));
+
+function addone(num){
+    return num + 1
+}
+
+// console.log(addOne(6));
+
+const addOne = function(num){
+    return num + 2
+}

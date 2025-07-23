b004ba09afa7196f501c576f5550722503f9b699
Siddharth Chudasama <168630164+Sid-9510@users.noreply.github.com>
2025-07-23 07:14:10 +0000

    Arrow function and iimplicit return

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -31,3 +31,20 @@ const chai =  () => {
     let username = "hitesh"
     console.log(this);
 }
+
+// chai();
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+    
+// }
+
+// const addTwo = (num1, num2) => num1 + num2
+// const addTwo = (num1, num2) => (num1 + num2)
+const addTwo = (num1, num2) => ({username : "Siddharth"})
+
+// console.log(addTwo(5, 6));
+
+const array = [2,5,8,7,4,1,3]
+
+array.forEach()
